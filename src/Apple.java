8b39db7876b1b656c2eb36d6b2093eb7e0307a0b
Amar Mujumdar <amarmujumdar12@gmail.com>
2025-04-06 19:59:27 -0700

    Added time constraints

---
+++ src/Apple.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.lang.System;
+
+public class Apple {
+    private int x;
+    private int y;
+
+    private double startTime;
+    private double timeLimit;
+    private double endTime;
+
+    int appleCounter; // when the snake eats the apple then the the apple counter will be affected by one, this is added inside.
+
+    Random rand = new Random();
+
+    public Apple() {
+        x = rand.nextInt(512);
+        y = rand.nextInt(512);
+
+        startTime = System.currentTimeMillis();
+        endTime = 30;
+        StdDraw.picture(x, y, "Apple.png");
+    }
+}
