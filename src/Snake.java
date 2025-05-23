680c9a37dcc70fcd6575c7bdda6b9fb87f51b13d
Amar Mujumdar <amarmujumdar12@gmail.com>
2025-05-22 20:23:49 -0700

    make chacker func

--- src/Snake.java
+++ src/Snake.java
@@ -1,10 +1,19 @@
 import java.applet.Applet;
+import java.awt.event.KeyEvent;
 
 public class Snake extends Applet {
     private final int WIDTH = 512;
     private final int HEIGHT = 512;
 
-    int[] xIntegers = new int[]
+    private int[] xIntegers = new int[WIDTH*HEIGHT];
+    private int[] yIntegers= new int[WIDTH*HEIGHT];
+
+    private final int left = KeyEvent.VK_LEFT;
+    private final int up = KeyEvent.VK_UP;
+    private final int down = KeyEvent.VK_DOWN;
+    private final int right = KeyEvent.VK_RIGHT;
+
+    private boolean inGame = true;
 
     public Snake() {
 
@@ -16,6 +25,15 @@ public class Snake extends Applet {
     }
 
     public void gameStart() {
+        xIntegers[0] = 256;
+        yIntegers[0] = 256;
 
+        Apple[] appleCoordinates = new Apple[20];
+
+        while (inGame) {
+            if (StdDraw.isKeyPressed(left) || StdDraw.isKeyPressed(right) || StdDraw.isKeyPressed(up) || StdDraw.isKeyPressed(down)) {
+                // make checks function
+            }
+        }
     }
 }
